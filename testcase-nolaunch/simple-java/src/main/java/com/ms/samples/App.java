57df2fa6bcb2c04207872fe1eb2ae7d68f051018
Mona Sun (CSI Interfusion Inc) <v-sunxi@microsoft.com>
2018-04-10 10:11:45 +0800

    Project with no launch.json

---
+++ testcase-nolaunch/simple-java/src/main/java/com/ms/samples/App.java
@@ -0,0 +1,19 @@
+package com.ms.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        int i = 0;
+        for (; i <= 1000; i++) {
+            if (i == 1000)  {
+                System.out.println( "Hello World!" );        
+            }
+        }
+        System.out.println( "Hello World!" );
+    }
+} 
