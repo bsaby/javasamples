021b6d25783710445d4ea5ba473ad0559aaccb12
Timoluo <JobA22@163.com>
2018-04-10 23:23:42 -0700

    Update Helloworld

--- testcase-nolaunch/1.helloworld/src/main/java/HelloWorld.java
+++ testcase-nolaunch/1.helloworld/src/main/java/HelloWorld.java
@@ -1,13 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("hello");
-        if (args != null && args.length > 0) {
-            for (String arg : args) {
-                System.out.print(" " + arg);
-            }
-            System.out.println();
-        } else {
-            System.out.println(" world");
-        }        
+public class App 
+{
+    public static void main( String[] args )
+    {        
+        System.out.println( "Hello World!" );
     }
 }
